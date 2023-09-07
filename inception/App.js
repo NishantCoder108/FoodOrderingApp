917f229d457976ec403424ba2b539401858239fe
Nishant Kumar <nishantcoder108@gmail.com>
2023-09-07 14:37:34 +0530

    React.createElement nested inside nested elelement

--- inception/App.js
+++ inception/App.js
@@ -14,11 +14,18 @@
 //   "Hello World from React CDN"
 // );
 
-const parent = React.createElement(
-  "h1",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, "Child Element")
-);
+const parent = React.createElement("h1", { id: "parent" }, "Parent Element", [
+  React.createElement("div", { id: "child" }, "Children Element 1", [
+    // for more than nested children in parent for that we use array []
+    React.createElement("h2", { id: "child1" }, "Child 1"),
+    React.createElement("h2", { id: "child1" }, "Child 2"),
+  ]),
+  React.createElement("div", { id: "child" }, "Children Element 2", [
+    // for more than nested children in parent for that we use array []
+    React.createElement("h2", { id: "child1" }, "Child 1"),
+    React.createElement("h2", { id: "child1" }, "Child 2"),
+  ]),
+]);
 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
