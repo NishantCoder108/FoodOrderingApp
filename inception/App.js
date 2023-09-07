d5e29cfd7b92d7a3af6c2e39f726cee7253d53ac
Nishant Kumar <nishantcoder108@gmail.com>
2023-09-07 14:30:07 +0530

    make one nested element

--- inception/App.js
+++ inception/App.js
@@ -1,10 +1,26 @@
-const heading = React.createElement(
+/**
+ *
+ * const parent = React.createElement("h1",{id:"parent"},React.createElement("div",{id:"child"}, "Child Element"))
+ *
+ *
+ *
+ *
+ *
+ */
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading",xyz:"abc" }, //atttribute
+//   "Hello World from React CDN"
+// );
+
+const parent = React.createElement(
   "h1",
-  { id: "heading" },
-  "Hello World from React CDN"
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, "Child Element")
 );
 
-console.log(heading);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
