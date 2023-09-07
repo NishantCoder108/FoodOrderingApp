9b8be62c6fc2dab87f78fcfa14143bf0230caa61
Nishant Kumar <nishantcoder108@gmail.com>
2023-09-07 14:42:38 +0530

    Create Nested Element inside react

--- inception/App.js
+++ inception/App.js
@@ -1,29 +1,11 @@
-/**
- *
- * const parent = React.createElement("h1",{id:"parent"},React.createElement("div",{id:"child"}, "Child Element"))
- *
- *
- *
- *
- *
- */
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading",xyz:"abc" }, //atttribute
-//   "Hello World from React CDN"
-// );
-
-const parent = React.createElement("h1", { id: "parent" }, "Parent Element", [
-  React.createElement("div", { id: "child" }, "Children Element 1", [
-    // for more than nested children in parent for that we use array []
-    React.createElement("h2", { id: "child1" }, "Child 1"),
-    React.createElement("h2", { id: "child1" }, "Child 2"),
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h2", {}, "Child 1"),
+    React.createElement("h2", {}, "Child 2"),
   ]),
-  React.createElement("div", { id: "child" }, "Children Element 2", [
-    // for more than nested children in parent for that we use array []
-    React.createElement("h2", { id: "child1" }, "Child 1"),
-    React.createElement("h2", { id: "child1" }, "Child 2"),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h2", {}, "Child 1"),
+    React.createElement("h2", {}, "Child 2"),
   ]),
 ]);
 
