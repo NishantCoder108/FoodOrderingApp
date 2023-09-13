a4f29c48e048133660975f2f2eab0e9b8052413b
Nishant Kumar <nishantcoder108@gmail.com>
2023-09-13 17:32:32 +0530

    food ordering app, structured design completed

--- App.js
+++ App.js
@@ -1,19 +1,89 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => {
-  return <h1>React Title </h1>;
+/**
+ * App Layout
+ *   - Header
+ *      - Logo , Home , Menu , ContactUs
+ *   - Body
+ *      - Restaurant Body
+ *         - Res Card
+ *            - Name ,Image , Stars, Res, Address
+ *   - Footer
+ */
+
+const Header = () => {
+  return (
+    <div className="nav">
+      <div>
+        <img
+          alt="logo"
+          className="logo"
+          src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-food-logo-designs-with-spoon-and-fork-png-image_4158238.jpg"
+        />
+      </div>
+      <div className="nav-menu">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const ReactComponent = () => (
-  <div>
-    <Title />
-    React ReactComponent
-  </div>
-);
+const ResCard = () => {
+  return (
+    <div className="res-card">
+      <div className="res-img-card-wrapper">
+        <img
+          className="res-card-img"
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/np13tsnfrxylkrk1ykm5"
+        />
+      </div>
+      <h3>Haldiram Bhujiawala</h3>
+      <h3>⭐ 4.5</h3>
+      <h4>
+        Sweets, North Indian, Chinese, South Indian, Fast Food, Snacks, Italian
+        Phulwari Sharif
+      </h4>
+    </div>
+  );
+};
+const RestaurantBody = () => {
+  return (
+    <div className="res-body">
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+      <ResCard />
+    </div>
+  );
+};
 
-// React Element | JSX Element
-const jsxHeading = <h1 id="heading">Namaste React 🚀</h1>;
-
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h5>Copyright ©️ 2023 - 2024</h5>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <RestaurantBody />
+      <Footer />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ReactComponent />);
+root.render(<AppLayout />);
