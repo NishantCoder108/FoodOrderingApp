a9a333a4c4290cee27026a4332a590b4fe5a0588
Nishant Kumar <nishantcoder108@gmail.com>
2023-09-13 13:48:58 +0530

    JSX is like HTML syntax, NOT HTML

--- App.js
+++ App.js
@@ -4,5 +4,14 @@ import ReactDOM from "react-dom/client";
 //React.createElement ===> Object ===> render(HTML Element)
 const heading = React.createElement("h1", { id: "heading" }, "Heading");
 
+// JSX is like HTML element , not HTML element
+
+/**
+ * Babel convert JSX to Pure React Element .it also called Transpiler or JS Compiler that oldest browser can understand
+ *
+ */
+
+const jsxHeading = <h1 id="heading">Namaste React 🚀</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
