2b2fd18f51f97cac25b31cf07f577d80d862c2bc
Nishant Kumar <nishantcoder108@gmail.com>
2023-09-17 11:03:33 +0530

    Convert Functional component to Class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,29 @@
+import { Component } from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div className="about">
-      <div>
-        <h2>Nishant Kumar</h2>
-        <h2>Web Engineer</h2>
-        <h2> Frontend Expert | React | JavaScript | TypeScript </h2>
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+  render() {
+    console.log("Parent Render");
+    return (
+      <div className="about">
+        <div>
+          <h2>Nishant Kumar</h2>
+          <h2>Web Engineer</h2>
+          <h2> Frontend Expert | React | JavaScript | TypeScript </h2>
+        </div>
+
+        <UserClass name="Akshay" course="JavaScript" />
+        {/* <UserClass name="Elon" course="Spaceship" /> */}
       </div>
-
-      <UserClass name="Akshay" course="JavaScript" />
-      <UserClass name="Elon" course="Spaceship" />
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default About;
