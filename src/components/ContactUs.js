1a0c15aecb9c29e6ab260d1fe4b98d2924f94866
Nishant Kumar <nishantcoder108@gmail.com>
2023-09-21 14:34:49 +0530

    contact us page update

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,9 +5,35 @@ import UserContext from "../utils/userContext";
 const ContactUs = () => {
   const { loggedInUser } = useContext(UserContext);
   return (
-    <div className="text-center">
-      <h1>Name : {loggedInUser} </h1>
-      <UserClass />
+    <div className="text-justify">
+      <pre>
+        {`
+Hey,  I'm Nishant  
+In this project, we've used all the important React concepts. We use Parcel to bundle our code neatly and React Router DOM to handle page navigation smoothly.
+
+For managing data across the app, we rely on React's Context API, which helps us share data where needed.
+We also make use of props and higher-order components to keep our code organized and flexible.
+
+To manage the overall state of the app effectively, we use Redux Toolkit.
+And for making our project look good and user-friendly, we've styled it using Tailwind CSS, ensuring a sleek design and responsiveness.
+
+All these technologies work together to create a fantastic About page that showcases the best of our project in a simple and engaging way.
+`}
+      </pre>
+      <h5 className="mt-3">
+        Feel free to reach out to me on
+        {
+          <a
+            className="font-extrabold underline text-[#00a9ff]"
+            href="https://github.com/NishantCoder108"
+          >
+            {" "}
+            GitHub{" "}
+          </a>
+        }
+        for any inquiries or collaboration opportunities. Your feedback and
+        contributions are highly appreciated! 🚀{" "}
+      </h5>
     </div>
   );
 };
